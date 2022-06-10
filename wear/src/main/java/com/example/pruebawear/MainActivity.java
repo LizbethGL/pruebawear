ad13750347a55d93982ed281d3b93281b8e95776
lizgl <lizbethgl.ti19@utsjr.edu.mx>
2022-06-10 13:35:54 -0500

    entrega 3:Actualizacion de notificacion

--- wear/src/main/java/com/example/pruebawear/MainActivity.java
+++ wear/src/main/java/com/example/pruebawear/MainActivity.java
@@ -15,9 +15,13 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.example.pruebawear.databinding.ActivityMainBinding;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
 
     private Button wBotton = null;
+    private Button wBoton;
 
     private TextView mTextView;
     private ActivityMainBinding binding;
@@ -28,11 +32,13 @@ public class MainActivity extends Activity {
     private NotificationManagerCompat nm;
     private NotificationCompat.WearableExtender wearableExtender;
 
-
+    String idChanel = "Mi_Canal";
     String idChannel = "Mi_Canal";
     int idNotification = 001;
 
     private NotificationCompat.BigTextStyle bigTextStyle;
+
+
 
     String longText = "with BigStyle, only a single line of text would be visible" +
             "Any additional text would not appear directly in the notification";
@@ -45,6 +51,7 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        wBoton = findViewById(R.id.wButton);
         wBotton = findViewById(R.id.wButton);
 
         intent = new Intent(MainActivity.this, MainActivity.class);
@@ -54,32 +61,72 @@ public class MainActivity extends Activity {
         wearableExtender = new NotificationCompat.WearableExtender();
         bigTextStyle = new NotificationCompat.BigTextStyle();
 
+
         wBotton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+                public void start(View view){
+                Timer t = new Timer();
+                TimerTask tt = new TimerTask() {
+                    @Override
+                    public void run() {
+                        int importance = NotificationManager.IMPORTANCE_HIGH;
+                        String name = "Notification 2";
+                        NotificationChannel notificationChannel = new NotificationChannel(idChannel, name, importance);
+
+                        nm.createNotificationChannel(notificationChannel);
+
+                        pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+                        notofication = new NotificationCompat.Builder(MainActivity.this, idChannel)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("Mi notification")
+                                .setContentText("Mi segunda notificacion wear")
+                                .extend(wearableExtender);
+                        nm.notify(idNotification, notofication.build());
+                    }
+                };
+
+                t.schedule(tt, 5000);
+
+            }
+
+                @Override
+                public void onClick (View view){
+                start(view);
                 int importance = NotificationManager.IMPORTANCE_HIGH;
                 String name = "Notificacion";
 
                 NotificationChannel notificationChannel =
-                        new NotificationChannel(idChannel, name, importance);
+                        new NotificationChannel(idChanel, name, importance);
+                String name1 = "Notification";
+                NotificationChannel notificationChannel1 = new NotificationChannel(idChannel, name, importance);
 
                 nm.createNotificationChannel(notificationChannel);
 
+                pendingIntent = pendingIntent.getActivity(MainActivity.this, 0, intent, 0);
                 pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
 
-                notofication = new NotificationCompat.Builder(MainActivity.this, idChannel)
-
+                /*notification = new NotificationCompat.Builder(MainActivity.this, idChanel)
+                notification = new NotificationCompat.Builder(MainActivity.this, idChannel)
                         .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("Notification Wear")
+                        .setContentTitle("Notificacion Wear")
                         .setContentText(longText)
                         .setContentIntent(pendingIntent)
                         .extend(wearableExtender)
-                        .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                        .setVibrate(new long[]{100,200,300,400,500,300,200,400})
                         .setStyle(bigTextStyle);
+                nm.notify(idNotification, notification.build());*/
 
+                notofication = new NotificationCompat.Builder(MainActivity.this, idChanel)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Mi notificacion")
+                        .setContentTitle("Mi notification")
+                        .setContentText("Mi primera notificacion wear")
+                        .extend(wearableExtender);
                 nm.notify(idNotification, notofication.build());
             }
+            });
 
-        });
+        }
     }
-}
+
